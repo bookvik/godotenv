1ce1daa9df6567a3fa294d74a776cb49c4032bad
Vitaliy Katorgin <vitalykatorgin@gmail.com>
2024-09-02 05:41:31 -0300

    Update godotenv.go

--- godotenv.go
+++ godotenv.go
@@ -20,7 +20,7 @@ import (
 	"os"
 	"os/exec"
 	"sort"
-	"strconv"
+	// "strconv"
 	"strings"
 )
 
@@ -164,11 +164,11 @@ func Write(envMap map[string]string, filename string) error {
 func Marshal(envMap map[string]string) (string, error) {
 	lines := make([]string, 0, len(envMap))
 	for k, v := range envMap {
-		if d, err := strconv.Atoi(v); err == nil {
-			lines = append(lines, fmt.Sprintf(`%s=%d`, k, d))
-		} else {
+		// if d, err := strconv.Atoi(v); err == nil {
+		// 	lines = append(lines, fmt.Sprintf(`%s=%d`, k, d))
+		// } else {
 			lines = append(lines, fmt.Sprintf(`%s="%s"`, k, doubleQuoteEscape(v)))
-		}
+		// }
 	}
 	sort.Strings(lines)
 	return strings.Join(lines, "\n"), nil
